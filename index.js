6236c49264808e713feba2c9f4aa367e8fbe3cc9
ajkarimi7 <ajkarimi7@gmail.com>
2019-05-02 08:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   const line = [];
   for (let i = 0; i < katzDeliLine.length; i++) {
-    line.push(` `+)
+    line.push(` `+[i+1]+)
   }
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
